b3c8e6fab0dfaf6f5395f902151457132e1eb6e5
solita-haskell <solita-haskell>
2016-12-02 20:05:50 +0000

    some haskell dev-tools packages to shell

--- shell.sh
+++ shell.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 nix-shell -p cabal2nix --command 'cabal2nix --shell . > shell.nix'
-nix-shell -p bashInteractive cabal-install --command 'nix-shell'
+nix-shell -p bashInteractive cabal-install -p haskellPackages.stylish-haskell -p haskellPackages.hdevtools -p haskellPackages.ghc-mod -p haskellPackages.hoogle -p haskellPackages.hasktags -p haskellPackages.apply-refact -p haskellPackages.pointfree -p ctags -p haskellPackages.scan --command 'nix-shell'
