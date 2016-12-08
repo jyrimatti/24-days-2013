19f67ee1bae67438e69cac556bb50bb83aa17b38
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-12-08 23:13:57 +0200

    let's generate default.nix so we can override it with shell.nix when needed

--- shell.sh
+++ shell.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/d5fd65df4e84b2d06606c70a5e7ee39daf1ddc73.tar.gz
 
-nix-shell -p cabal2nix --command 'cabal2nix --shell . > shell.nix'
+nix-shell -p cabal2nix --command 'cabal2nix --shell . > default.nix'
 nix-shell -p bashInteractive --command "nix-shell --command 'nix-shell -p bashInteractive -p cabal-install -p haskellPackages.stylish-haskell -p haskellPackages.hdevtools -p haskellPackages.hsdev -p haskellPackages.ghc-mod -p haskellPackages.hoogle -p haskellPackages.hasktags -p haskellPackages.pointfree -p ctags -p haskellPackages.scan -p haskellPackages.hdocs'"
