f441f77a7b890d04baa8bdbc0923bd84e79eb4fc
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-12-16 19:37:14 +0200

    14-acid-state

---
+++ 14-acid-state/default.nix
@@ -0,0 +1,30 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, acid-state, base, containers, mtl, safecopy
+      , stdenv, time
+      }:
+      mkDerivation {
+        pname = "x14-acid-state";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          acid-state base containers mtl safecopy time
+        ];
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
