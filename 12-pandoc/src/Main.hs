206adad9f1c5fa08120408ed11485e397655bc61
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-12-16 18:52:33 +0200

    12-pandoc

--- 12-pandoc/src/Main.hs
+++ 12-pandoc/src/Main.hs
@@ -1,4 +1,29 @@
 module Main where
 
+import Text.Pandoc
+
+textToConvert = unlines [ "Hello World."
+                        , ""
+                        , "    this is a Markdown code block"
+                        , ""
+                        , "[This is a link](http://www.latermuse.com/)" ]
+
+
+Right pandocParsed = readMarkdown def textToConvert
+
+pandocConverted = writeLaTeX def pandocParsed
+convertedToHtml = writeHtmlString def pandocParsed
+convertedToOpenDocument = writeOpenDocument opts pandocParsed
+  where
+    opts = def { writerWrapText = WrapAuto -- Enable text wrapping
+               , writerColumns = 80 }  -- Set column width to 80
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+	print pandocParsed
+	putStrLn ""
+	putStrLn pandocConverted
+	putStrLn ""
+	putStrLn convertedToHtml
+	putStrLn ""
+	putStrLn convertedToOpenDocument
