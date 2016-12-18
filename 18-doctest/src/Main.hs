ac35f797d883c349c9f5162026e148f189a9947c
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-12-18 22:38:41 +0200

    18-doctest

--- 18-doctest/src/Main.hs
+++ 18-doctest/src/Main.hs
@@ -1,4 +1,28 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import Test.DocTest
+
+{-| Given an integer, 'square' returns the same number squared:
+>>> square 5
+25
+
+prop> \x -> square x == x+x
+-}
+square :: Int -> Int
+square x = x * x
+
+{-|
+>>> :{
+      let callback name = do
+            putStrLn $ "Hello. Yes, this is " ++ name
+>>> :}
+>>> printer "Dog" callback
+Dog says:
+Hello. Yes, this is Dog
+-}
+printer :: String -> (String -> IO ()) -> IO ()
+printer name callBack = do
+  putStrLn $ name ++ " says:"
+
+main  :: IO ()
+main = doctest [ "src/Main.hs" ]
